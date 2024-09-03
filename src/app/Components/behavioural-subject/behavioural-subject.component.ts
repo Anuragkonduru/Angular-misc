ec93559bad8bb9d389905518e29cd0a4fcc9f0ac
Anurag <konduruanurag@gmail.com>
2024-09-03 10:06:42 +0530

    Init

---
+++ src/app/Components/behavioural-subject/behavioural-subject.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-behavioural-subject',
+  standalone: true,
+  imports: [],
+  templateUrl: './behavioural-subject.component.html',
+  styleUrl: './behavioural-subject.component.css'
+})
+export class BehaviouralSubjectComponent {
+
+}
---
+++ src/app/Components/http-interceptor/http-interceptor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-http-interceptor',
+  standalone: true,
+  imports: [],
+  templateUrl: './http-interceptor.component.html',
+  styleUrl: './http-interceptor.component.css'
+})
+export class HttpInterceptorComponent {
+
+}
